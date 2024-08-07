c80b0fd93a5f9693ed460bf9ab4ab867e4f79322
lovin4ik <rasteniaros@gmail.com>
2024-08-07 18:56:01 +0300

    first commit

---
+++ js/modal.js
@@ -0,0 +1,24 @@
+const backdrop = document.querySelector('.backdrop')
+const modal = document.querySelector('.modal')
+const modalCloseBtn = document.querySelector('.modal__close')
+const modalOpenBtn = document.querySelector('.hero__button')
+
+const toggleModal = () => {
+	backdrop.classList.toggle('is-hidden')
+	document.body.classList.toggle('no-scroll')
+}
+
+modalOpenBtn.addEventListener('click', toggleModal)
+modalCloseBtn.addEventListener('click', toggleModal)
+backdrop.addEventListener('click', e => {
+	backdrop.classList.remove('is-hidden')
+	document.body.classList.add('no-scroll')
+})
+modal.addEventListener('click', e => e.stopPropagation())
+
+window.addEventListener('keydown', e => {
+	if (e.key === 'Escape') {
+		backdrop.classList.add('is-hidden')
+		document.body.classList.remove('no-scroll')
+	}
+})
